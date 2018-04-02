37fd223fd038b7b4effe524b46d8efa92b388795
giangmt <maitruonggiangbkhn@gmail.com>
2018-04-02 15:02:46 +0700

    signup-login-logout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,6 @@
 module ApplicationHelper
-  def full_title(page_title = '')                     
-    base_title = "Library Managerment"
-    if page_title.empty?              
-      base_title                      
-    else
-      page_title + " | " + base_title 
-    end
+  def full_title page_title = " "
+    base_title = t ".title"
+    page_title.empty? ? base_title : page_title + " | " + base_title
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root "books#index"
 
-  get "static_pages/home"
-  get "static_pages/help"
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
   resources :books
+  resources :users
 end
